7654fd85d9b374c9093ae14c94b67de3b5766f07
ItDage <1418332683@qq.com>
2018-09-12 23:56:53 +0800

    chat连接时保存关系  方便后来判断推送位置

--- springboot/src/main/java/com/itdage/websocket/chat/ChatHandler.java
+++ springboot/src/main/java/com/itdage/websocket/chat/ChatHandler.java
@@ -12,22 +12,17 @@ public class ChatHandler extends AbstractWebSocketHandler {
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		// TODO Auto-generated method stub
-		super.afterConnectionEstablished(session);
+		
 		System.out.println("chat连接成功");
 	}
 
 	@Override
 	public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-		// TODO Auto-generated method stub
-		super.handleMessage(session, message);
 		session.sendMessage(new TextMessage("chat服务端响应"));
 	}
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		// TODO Auto-generated method stub
-		super.afterConnectionClosed(session, status);
 		System.out.println("chat连接关闭");
 	}
 }
