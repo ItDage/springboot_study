d480d1d6bb4dd6e55569f1cb4b81204ba382d131
sunchuiyou <sunchuiyou@thunisoft.com>
2018-10-09 19:22:59 +0800

    设置访问路径

--- springboot/src/main/java/com/itdage/constant/StatusConstant.java
+++ springboot/src/main/java/com/itdage/constant/StatusConstant.java
@@ -1,8 +1,5 @@
 package com.itdage.constant;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
