9136b539fa32efc65fdd066e4550668fa37d2fce
sunchuiyou <sunchuiyou@thunisoft.com>
2018-10-10 20:36:34 +0800

    乱改

--- springboot/src/main/java/com/itdage/controller/LoginController.java
+++ springboot/src/main/java/com/itdage/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.itdage.entity.Result;
 
@@ -20,25 +21,41 @@ public class LoginController {
 	public String loginHtml() {
 		return "login-new";
 	}
-	@ResponseBody
+//	@ResponseBody
+//	@RequestMapping(value = "/login", method = RequestMethod.POST)
+//	public Result login(String username, String password, HttpServletRequest request) {
+//		Result result = new Result();
+//			if (userMap.containsKey(username)) {
+//				if(userMap.get(username).equals(password)){
+//					request.getSession().setAttribute("username", username);
+//					result.setMsg("登录成功");
+//					result.setCode(200);
+//				}else{
+//					result.setCode(400);
+//					result.setMsg("用户名或密码不正确!");
+//				}
+//			} else {
+//				result.setCode(200);
+//				result.setMsg("注册成功");
+//				userMap.put(username, password);
+//				request.getSession().setAttribute("username", username);
+//			}
+//			return result;
+//		}
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public Result login(String username, String password, HttpServletRequest request) {
-		Result result = new Result();
+	public String login(String username, String password, HttpServletRequest request, RedirectAttributes attribute) {
 			if (userMap.containsKey(username)) {
 				if(userMap.get(username).equals(password)){
 					request.getSession().setAttribute("username", username);
-					result.setMsg("登录成功");
-					result.setCode(200);
 				}else{
-					result.setCode(400);
-					result.setMsg("用户名或密码不正确!");
+					attribute.addFlashAttribute("code", 404);
+					attribute.addFlashAttribute("msg", "用户名或密码不正确!");
+					return "redirect:/login-new.html";
 				}
 			} else {
-				result.setCode(200);
-				result.setMsg("注册成功");
 				userMap.put(username, password);
 				request.getSession().setAttribute("username", username);
 			}
-			return result;
+			return "index";
 		}
 }
